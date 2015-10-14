f41173205fed54c1a82517d350d37573598ca427
CelyaRousseau <celyarousseau.pro@gmail.com>
2015-10-14 08:46:20 +0200

    add palindrome

--- mots_croises.pl
+++ mots_croises.pl
@@ -6,9 +6,10 @@ nombreSuivant(X, S) :- S is X + 1.
 
 nombreAyantDizaine(X, N) :- Low is X*10, High is X*10+9, between(Low, High, N).
 
+palindrome(X) :- number_chars(X, L), reverse(L, R), compare(=, L, R).
+
 
 /** TODO **/
 
-/** CALCULER -> Liste(s) + Récursivité **/ 
+/** CALCULER -> Liste(s) + Récursivité **/
 /** CUBE **/
-/** PALINDROME **/
